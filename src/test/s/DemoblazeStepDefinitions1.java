ac2c08f3fac977cfbdf274dd56e20c431c03b714
alla <akhnv@yahoo.com>
2025-03-21 15:43:57 +0200

    updates to the cucumber tests implementation

--- src/test/s/DemoblazeStepDefinitions1.java
+++
@@ -1,5 +0,0 @@
-public class DemoblazeStepDefinitions1 {
-    @io.cucumber.java.en.When("user enters valid data in the form fields:")
-    public void userEntersValidDataInTheFormFields() {
-    }
-}
